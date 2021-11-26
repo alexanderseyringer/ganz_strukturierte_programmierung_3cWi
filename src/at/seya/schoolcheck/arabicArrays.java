60254cb9337ffc54b358098fefadf1eb71a94165
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2021-11-26 12:02:03 +0100

    Added new Package -Schoolchecks-

---
+++ src/at/seya/schoolcheck/arabicArrays.java
@@ -0,0 +1,41 @@
+package at.seya.schoolcheck;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
+public class arabicArrays {
+    public static void main(String[] args) {
+        boolean finished = false;
+
+        while(finished == false){
+            Scanner scammer = new Scanner(System.in);
+            Random randomizer2000 = new Random();
+            System.out.println("Hallo! Bitte gib an wie viele Sätze du gerne hättest! (Achtung: Es sind nur Zahlen erlaubt!)");
+
+            int sentencesCount = scammer.nextInt();
+
+            // Danke Michael
+            String[] anrede = {"Er ", "Sie "};
+            String[] action = {"geht ", "läuft ", "rennt ", "fährt "};
+            String[] location = {"nach Hause.", "zur Schule.", "ins Büro.", "ins Puff."}; //Eventuell Jugendfreundlicher machen
+
+            for (int i = 0; i < sentencesCount; i++) {
+                int randomizerAnrede = randomizer2000.nextInt(2);
+                int randomizerAction = randomizer2000.nextInt(4);
+                int randomizerLocation = randomizer2000.nextInt(4);
+                System.out.println(anrede[randomizerAnrede] + action[randomizerAction] + location[randomizerLocation]);
+            }
+
+            System.out.println("Noch eine Runde (1) oder lieber aufhören (2)?");
+
+            int playAgain = scammer.nextInt();
+
+            if(playAgain == 2) {
+                finished = true;
+            }
+        }
+
+    }
+
+}
--- src/at/seya/strukturierteProgrammierung/arrays/HangMan.java
+++ src/at/seya/strukturierteProgrammierung/arrays/HangMan.java
@@ -10,21 +10,44 @@ public class HangMan {
             geheimeWort += "*";
         }
 
-          char[] preselectedWord = ersteWort.toCharArray(); // Zeug ausm String in ein Array schreiben
-        for (int i = 0; i < preselectedWord.length; i++) {
+         char[] preselectedWord = ersteWort.toCharArray(); // Zeug ausm String in ein Array schreiben
+          /*  for (int i = 0; i < preselectedWord.length; i++) {
            // System.out.print(preselectedWord[i]);     //Inhalt vom Array ausgeben -> Nur für Testzwecke
-        }
+        } */
 
         Scanner scammerinput = new Scanner(System.in);
 
+        boolean win = false;
+        char userInput;
+
         // Hier irgendwie nen loop hinklatschen oder so
 
-        System.out.println("Herzlichen Willkommen bei Hangman!");
-        System.out.println("Es ist ganz einfach: die Sterne stehen jeweils für einen Buchstaben und du must nun in 6 Runden das Wort erraten, sonst hast du Verloren");
+        while(win == false){
+            System.out.println("Herzlichen Willkommen bei Hangman!");
+            System.out.println("Es ist ganz einfach: die Sterne stehen jeweils für einen Buchstaben und du must nun in 6 Runden das Wort erraten, sonst hast du Verloren");
 
-        System.out.println(geheimeWort);
+            System.out.println(geheimeWort);
 
-        System.out.println("Bitte gib nun deinen Lösungsansatz ein!");
+            System.out.println("Bitte gib nun deinen Lösungsansatz ein!");
+
+            userInput = scammerinput.next().charAt(0);
+           // System.out.println(userInput); //Ausgabe ob der Lachs löppt
+
+            // char[] userGuess = userInput.toCharArray();
+           /* for (int i = 0; i < userGuess.length; i++) {
+                System.out.println(userGuess[i]);
+            } */
+
+            for (int i = 0; i < preselectedWord.length; i++) {
+                if (preselectedWord[i] == userInput){
+
+                    System.out.println("Wenn des funktioniert friss i an besa");
+                };
+            }
+
+        }
+
+
 
 
 
