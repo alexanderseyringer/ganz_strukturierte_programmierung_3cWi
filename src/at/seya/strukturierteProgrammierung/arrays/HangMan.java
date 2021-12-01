eb781042607a9c7f83d073a1b1708b771d3a4461
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2021-12-01 08:24:02 +0100

    Finished structure of Hangman

--- src/at/seya/strukturierteProgrammierung/arrays/HangMan.java
+++ src/at/seya/strukturierteProgrammierung/arrays/HangMan.java
@@ -5,9 +5,10 @@ import java.util.Scanner;
 public class HangMan {
     public static void main(String[] args) {
         String ersteWort = "Peta";
-        String geheimeWort ="";
-        for (int i = 0; i < ersteWort.length(); i++) {
-            geheimeWort += "*";
+
+        char[] geheimeWort = new char[ersteWort.length()];
+        for (int i = 0; i < geheimeWort.length; i++) {
+            geheimeWort[i] = '*';
         }
 
         char[] preselectedWord = ersteWort.toCharArray(); // Zeug ausm String in ein Array schreiben
@@ -20,17 +21,23 @@ public class HangMan {
         boolean win = false;
         char userInput;
 
+        String ganzeWort = "";
+
         // Hier irgendwie nen loop hinklatschen oder so
 
         while(win == false){
             System.out.println("Herzlichen Willkommen bei Hangman!");
-            System.out.println("Es ist ganz einfach: die Sterne stehen jeweils für einen Buchstaben und du must nun in 6 Runden das Wort erraten, sonst hast du Verloren");
+            System.out.println("Es ist ganz einfach: die Sterne stehen jeweils für einen Buchstaben und du must nun in 6 Runden das Wort erraten, sonst hast du Verloren.");
 
-            System.out.println(geheimeWort);
+            for (int i = 0; i < geheimeWort.length; i++) {
+                System.out.print(geheimeWort[i]);
+            }
 
+            System.out.println(" ");
             System.out.println("Bitte gib nun deinen Lösungsansatz ein!");
 
             userInput = scammerinput.next().charAt(0);
+
             // System.out.println(userInput); //Ausgabe ob der Lachs löppt
 
             // char[] userGuess = userInput.toCharArray();
@@ -40,9 +47,22 @@ public class HangMan {
 
             for (int i = 0; i < preselectedWord.length; i++) {
                 if (preselectedWord[i] == userInput){
-
-                    System.out.println("Wenn des funktioniert friss i an besa");
+                    geheimeWort[i] = preselectedWord[i];
+                    // System.out.println("Wenn des funktioniert friss i an besa");
                 };
+            }
+
+            for (int ie = 0; ie < geheimeWort.length; ie++) {
+                ganzeWort = ganzeWort + geheimeWort[ie];
+            }
+
+            System.out.println(ganzeWort);
+            if(ganzeWort.contains("*")) {
+                ganzeWort = "";
+            }
+            else {
+                System.out.println("Du hast alles erraten! Glückwunsch!");
+                win = true;
             }
 
         }
