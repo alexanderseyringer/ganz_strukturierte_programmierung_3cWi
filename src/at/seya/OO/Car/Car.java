1a8c20cbd4d570b85490783abe40bb61ad3b8d0a
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2021-12-10 12:18:07 +0100

    Further Work on OO - Car

--- src/at/seya/OO/Car/Car.java
+++ src/at/seya/OO/Car/Car.java
@@ -5,6 +5,33 @@ public class Car {
     public String model;
     public String colour;
     public String fuelType;
-    public String fuelConsumption;
+    public int fuelConsumption;
+    public int fuelAmount;
+    public int fuelCapacity;
     public String serialNumber;
+
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("Dat Auto löppt!");
+    }
+
+    public void breaking(){
+        System.out.println("Ich Bremse");
+    }
+
+    public void ecoBoost(){
+        if(fuelAmount > 0.10*fuelCapacity){
+            System.out.println("Super Duper Boost Mode!");
+        }
+
+        else {
+            System.out.println("Not enough Fuel to go superduperboost!");
+        }
+    }
+
+    public void honk(int amountOfRepetitions){
+        for (int i = 0; i < amountOfRepetitions ; i++) {
+            System.out.println("Tuuuuuut");
+        }
+    }
 }
--- src/at/seya/OO/Car/Main.java
+++ src/at/seya/OO/Car/Main.java
@@ -9,7 +9,16 @@ public class Main {
         c1.model = "A4 55 TDI S-Line";
         c1.colour = "SnowWhite";
         c1.fuelType = "Diesel";
-        c1.fuelConsumption = "3";
+        c1.fuelConsumption = 3;
+        c1.fuelAmount = 65;
+        c1.fuelCapacity = 70;
         c1.serialNumber = "A987e";
+
+        c1.drive();
+        System.out.println("Aktueller Fuellstand: " + c1.fuelAmount);
+
+        c1.ecoBoost();
+
+        c1.honk(11);
     }
 }
