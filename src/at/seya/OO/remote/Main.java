e9f26cc07d7c8822bc3a4156b8210aec26ce983c
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-01-12 09:17:02 +0100

    Fixed issue due to unexpected braindamage

--- src/at/seya/OO/remote/Main.java
+++ src/at/seya/OO/remote/Main.java
@@ -1,10 +1,11 @@
 package at.seya.OO.remote;
 
 public class Main {
+    public static void main(String[] args) {
+        Battery b1 = new Battery("Lithium", 79);
 
-    Battery b1 = new Battery("Lithium", 79);
+        Remote r1 = new Remote();
 
-    Remote r1 = new Remote();
-
-    r1.addBatteries(b1);
+        r1.addBatteries(b1);
+    }
 }
