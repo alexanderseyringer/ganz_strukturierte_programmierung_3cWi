1fa6eed17f8b95b8f8ac459df301ee4977dd15eb
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-01-19 08:50:39 +0100

    revisited new consumption function

--- src/at/seya/OO/cars/Car.java
+++ src/at/seya/OO/cars/Car.java
@@ -7,14 +7,16 @@ public class Car {
     private double standardFuelConsumption;
     private Engine engine;
     private Producer producer;
+    int mileage;
 
-    public Car(String colour, int topSpeed, int standardPriceRecommendation, double standardFuelConsumption, Engine engine, Producer producer) {
+    public Car(String colour, int topSpeed, int standardPriceRecommendation, double standardFuelConsumption, Engine engine, Producer producer, int mileage) {
         this.colour = colour;
         this.topSpeed = topSpeed;
         this.standardPriceRecommendation = standardPriceRecommendation;
         this.standardFuelConsumption = standardFuelConsumption;
         this.engine = engine;
         this.producer = producer;
+        this.mileage = mileage;
     }
 
     public void discountedPrice() {
@@ -22,10 +24,13 @@ public class Car {
         System.out.println("Der Preis, nach abzug des Rabattes, beträgt: " + discountedPrice + "€");
     }
 
-    public void newConsumption(int mileage) {
-        if(mileage >= 50000) {
+    public void newConsumption() {
+        if(this.mileage >= 50000) {
             double newFuelConsumption = this.standardFuelConsumption + (this.standardFuelConsumption * 0.098);
             System.out.println("Der neue Spritverbrauch, nach 50.000km, beträgt: " + newFuelConsumption + "l/100km");
+        }
+        else {
+            System.out.println("Alles in Ordnung!");
         }
     }
 }
--- src/at/seya/OO/cars/Main.java
+++ src/at/seya/OO/cars/Main.java
@@ -7,13 +7,23 @@ public class Main {
 
         Engine e1 = new Engine(110, at.seya.OO.car.Engine.engineType.Gas);
 
+        Engine e2 = new Engine(365, at.seya.OO.car.Engine.engineType.Gas);
+
+        Producer audi = new Producer("Audi", "Germany", 0.10);
+
         Producer opel = new Producer("Opel", "Germany", 0.35);
 
-        Car astraST = new Car("magicSilver", 200, 32000,3.23, e1, opel);
+        Car rs6Avant = new Car("sunsetOrange", 350, 78000, 6.8, e2, audi, 10654);
+
+        Car astraST = new Car("magicSilver", 200, 32000,3.23, e1, opel, 65000);
 
         astraST.discountedPrice();
 
-        astraST.newConsumption(65000);
+        astraST.newConsumption();
+
+        rs6Avant.discountedPrice();
+
+        rs6Avant.newConsumption();
 
     }
 }
