7ca6a1b7db05d9ccba9f42e776aeb3ab1040a5e2
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-01-19 09:14:25 +0100

    Added new package calculator

---
+++ src/at/seya/OO/calculator/BasicCalculator.java
@@ -0,0 +1,5 @@
+package at.seya.OO.calculator;
+
+public class BasicCalculator {
+
+}
---
+++ src/at/seya/OO/calculator/Main.java
@@ -0,0 +1,7 @@
+package at.seya.OO.calculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
