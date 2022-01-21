ed2cf51f153d8f1bfa260982520031f35a00bc68
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-01-21 11:29:24 +0100

    refactored currency calculator due to changes of the assignment

--- src/at/seya/OO/calculator/currencyCalculatorLegacy.java
+++ src/at/seya/OO/calculator/currencyCalculatorLegacy.java
@@ -1,8 +1,8 @@
 package at.seya.OO.calculator;
 
-public class currencyCalculator extends BasicCalculator{
+public class currencyCalculatorLegacy extends BasicCalculator{
 
-    public currencyCalculator(String producer, String model, String color) {
+    public currencyCalculatorLegacy(String producer, String model, String color) {
         super(producer, model, color);
     }
 
