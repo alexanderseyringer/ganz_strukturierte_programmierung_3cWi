868a8dcd5138aae6547c0c7360cac7eee7daee18
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-02-04 12:15:35 +0100

    Added basic class camera

--- src/at/seya/OO/handy/Camera.java
+++ src/at/seya/OO/handy/Camera.java
@@ -1,4 +1,17 @@
 package at.seya.OO.handy;
 
 public class Camera {
+    private int resolution;
+
+    public Camera(int resolution) {
+        this.resolution = resolution;
+    }
+
+    public int getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(int resolution) {
+        this.resolution = resolution;
+    }
 }
