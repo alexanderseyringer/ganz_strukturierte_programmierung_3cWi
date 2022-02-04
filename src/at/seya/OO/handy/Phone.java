19df3979a5eec1163088191f74f10ec3594f42f8
Alexander Seyringer <67291598+alexanderseyringer@users.noreply.github.com>
2022-02-04 12:16:26 +0100

    added cosntructor

--- src/at/seya/OO/handy/Phone.java
+++ src/at/seya/OO/handy/Phone.java
@@ -1,4 +1,15 @@
 package at.seya.OO.handy;
 
 public class Phone {
+    private String color;
+    private SIM sim;
+    private Camera camera;
+    private SDCard sdCard;
+
+    public Phone(String color, SIM sim, Camera camera, SDCard sdCard) {
+        this.color = color;
+        this.sim = sim;
+        this.camera = camera;
+        this.sdCard = sdCard;
+    }
 }
